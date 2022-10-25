a211afcfb748c0c8097181dac3578e705f5c02c4
Hùng Đinh <hungngohihihi@gmail.com>
2022-10-26 00:11:53 +0700

    Create bai6_1ChoosingOption.java

---
+++ lab01/bai6_1ChoosingOption.java
@@ -0,0 +1,12 @@
+package lab01;
+import javax.swing.JOptionPane;
+
+public class bai6_1ChoosingOption {
+	public static void main(String[] args) {
+		int option = JOptionPane.showConfirmDialog(null, "Do you want to change to the first class ticket?");
+		
+		JOptionPane.showMessageDialog(null, "You've chosen: " + (option==JOptionPane.YES_OPTION?"I do":"I don't"));
+		
+		System.exit(0);
+	}
+}
