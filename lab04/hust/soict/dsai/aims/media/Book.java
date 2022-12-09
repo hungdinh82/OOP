bed41cf2ccc533d39de5c97737f1b7d423854692
Hùng Đinh <hungngohihihi@gmail.com>
2022-12-09 20:09:24 +0700

    commit to branch release/lab04

--- lab04/hust/soict/dsai/aims/media/Book.java
+++ lab04/hust/soict/dsai/aims/media/Book.java
@@ -1,10 +1,44 @@
 package hust.soict.dsai.aims.media;
 
-public class Book {
+import java.util.ArrayList;
+import java.util.List;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Book extends Media{
+    private List<String> authors = new ArrayList<String>();
 
-	}
+    public Book(String title) {
+        super(title);
+    }
 
+    public Book(String id, String title, String category, float cost) {
+        super(id, title, category, cost);
+    }
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(List<String> authors) {
+        this.authors = authors;
+    }
+
+    public void addAuthor(String authorName) {
+        if (authors.contains(authorName)) {
+            System.out.println("Author is already in the list.");
+            return;
+        }
+
+        System.out.println(authorName + "add");
+        authors.add(authorName);
+        System.out.println("Successfully added new author.");
+    }
+
+    public void removeAuthor(String authorName) {
+        if (!authors.contains(authorName)) {
+            System.out.println("Author is not in the list.");
+            return;
+        }
+        authors.remove(authorName);
+    }
 }
+
--- lab04/hust/soict/dsai/aims/media/Media.java
+++ lab04/hust/soict/dsai/aims/media/Media.java
@@ -1,5 +1,35 @@
 package hust.soict.dsai.aims.media;
 
-public class Media {
+public abstract class Media {
+    private String id;
+    private final String title;
+    private String category;
+    private float cost;
 
+    public Media(String title) {
+        this.title = title;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Media(String id, String title, String category, float cost) {
+        this(title);
+        this.id = id;
+        this.category = category;
+        this.cost = cost;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public float getCost() {
+        return cost;
+    }
 }
